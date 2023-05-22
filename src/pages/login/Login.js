5c7453399e09b6b66fdb492b3066fff228890965
connorb023 <connorcb@yahoo.com>
2023-05-22 17:20:23 -0600

    starter functions

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,3 +3,22 @@ import { useContext, useRef } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import "./login.css";
+
+export default function Login() {
+    const userRef = useRef();
+    const passwordRef = useRef();
+    const { dispatch, isFetching } = useContext(Context);
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      dispatch({ type: "LOGIN_START" });
+      try {
+        const res = await axios.post("/auth/login", {
+          username: userRef.current.value,
+          password: passwordRef.current.value,
+        });
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      } catch (err) {
+        dispatch({ type: "LOGIN_FAILURE" });
+      }
+    };
