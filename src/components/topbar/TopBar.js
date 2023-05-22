4f8a6b252b05735a6287e6baed94bf9d338cd63e
connorb023 <connorcb@yahoo.com>
2023-05-22 17:13:21 -0600

    top bar imports

---
+++ src/components/topbar/TopBar.js
@@ -0,0 +1,8 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import "./topbar.css";
+
+export default function TopBar() {
+  const { user, dispatch } = useContext(Context);
+  const PF = "http://localhost:5000/images/"
