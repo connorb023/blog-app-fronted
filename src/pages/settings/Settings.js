f1cdc2dd0ceca9b1e5be7f5775e9539bd280b97c
connorb023 <connorcb@yahoo.com>
2023-05-22 17:26:26 -0600

    file and update code

--- src/pages/settings/Settings.js
+++ src/pages/settings/Settings.js
@@ -13,4 +13,32 @@ export default function Settings() {
   
     const { user, dispatch } = useContext(Context);
     const PF = "http://localhost:9000/images/"
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      dispatch({ type: "UPDATE_START" });
+      const updatedUser = {
+        userId: user._id,
+        username,
+        email,
+        password,
+      };
+      if (file) {
+        const data = new FormData();
+        const filename = Date.now() + file.name;
+        data.append("name", filename);
+        data.append("file", file);
+        updatedUser.profilePic = filename;
+        try {
+          await axios.post("/upload", data);
+        } catch (err) {}
+      }
+      try {
+        const res = await axios.put("/users/" + user._id, updatedUser);
+        setSuccess(true);
+        dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
+      } catch (err) {
+        dispatch({ type: "UPDATE_FAILURE" });
+      }
+    };
   
