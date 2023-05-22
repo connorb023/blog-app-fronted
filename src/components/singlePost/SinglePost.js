79ee68bc41fa788bcfee3e0ff524caa2e05f474d
connorb023 <connorcb@yahoo.com>
2023-05-22 16:59:46 -0600

    template

--- src/components/singlePost/SinglePost.js
+++ src/components/singlePost/SinglePost.js
@@ -45,4 +45,63 @@ export default function SinglePost() {
           setUpdateMode(false)
         } catch (err) {}
       };
-    
+      return (
+        <div className="singlePost">
+          <div className="singlePostWrapper">
+            {post.photo && (
+              <img src={PF + post.photo} alt="" className="singlePostImg" />
+            )}
+            {updateMode ? (
+              <input
+                type="text"
+                value={title}
+                className="singlePostTitleInput"
+                autoFocus
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            ) : (
+              <h1 className="singlePostTitle">
+                {title}
+                {post.username === user?.username && (
+                  <div className="singlePostEdit">
+                    <i
+                      className="singlePostIcon far fa-edit"
+                      onClick={() => setUpdateMode(true)}
+                    ></i>
+                    <i
+                      className="singlePostIcon far fa-trash-alt"
+                      onClick={handleDelete}
+                    ></i>
+                  </div>
+                )}
+              </h1>
+            )}
+            <div className="singlePostInfo">
+              <span className="singlePostAuthor">
+                Author:
+                <Link to={`/?user=${post.username}`} className="link">
+                  <b> {post.username}</b>
+                </Link>
+              </span>
+              <span className="singlePostDate">
+                {new Date(post.createdAt).toDateString()}
+              </span>
+            </div>
+            {updateMode ? (
+              <textarea
+                className="singlePostDescInput"
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
+              />
+            ) : (
+              <p className="singlePostDesc">{desc}</p>
+            )}
+            {updateMode && (
+              <button className="singlePostButton" onClick={handleUpdate}>
+                Update
+              </button>
+            )}
+          </div>
+        </div>
+      );
+    }
