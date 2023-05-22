fced90e69379d53f59c44925828896d80b55d2ee
connorb023 <connorcb@yahoo.com>
2023-05-22 17:20:46 -0600

    template

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -22,3 +22,34 @@ export default function Login() {
         dispatch({ type: "LOGIN_FAILURE" });
       }
     };
+
+    return (
+        <div className="login">
+          <span className="loginTitle">Login</span>
+          <form className="loginForm" onSubmit={handleSubmit}>
+            <label>Username</label>
+            <input
+              type="text"
+              className="loginInput"
+              placeholder="Enter your username..."
+              ref={userRef}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              className="loginInput"
+              placeholder="Enter your password..."
+              ref={passwordRef}
+            />
+            <button className="loginButton" type="submit" disabled={isFetching}>
+              Login
+            </button>
+          </form>
+          <button className="loginRegisterButton">
+            <Link className="link" to="/register">
+              Register
+            </Link>
+          </button>
+        </div>
+      );
+    }
