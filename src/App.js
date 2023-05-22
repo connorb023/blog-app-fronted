188dcb8d0f933b0f476bb4190e404b9c48e68127
connorb023 <connorcb@yahoo.com>
2023-05-22 17:42:48 -0600

    fixes

--- src/App.js
+++ src/App.js
@@ -5,27 +5,23 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
 import { useContext } from "react";
 import { Context } from "./context/Context";
 
 function App() {
-  const { user } = useContext(Context);
+  const user = false;
   return (
     <Router>
-      <TopBar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/register">{user ? <Home /> : <Register />}</Route>
-        <Route path="/login">{user ? <Home /> : <Login />}</Route>
-        <Route path="/write">{user ? <Write /> : <Register />}</Route>
-        <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
-        <Route path="/post/:postId">
-          <Single />
-        </Route>
-      </Switch>
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
     </Router>
   );
 }
