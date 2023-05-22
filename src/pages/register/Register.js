56338909d7b80b70ce0195bc9d5fe6e6c0d452d2
connorb023 <connorcb@yahoo.com>
2023-05-22 17:22:59 -0600

    template

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -23,3 +23,41 @@ export default function Register() {
         setError(true);
       }
     };
+    return (
+        <div className="register">
+          <span className="registerTitle">Register</span>
+          <form className="registerForm" onSubmit={handleSubmit}>
+            <label>Username</label>
+            <input
+              type="text"
+              className="registerInput"
+              placeholder="Enter your username..."
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label>Email</label>
+            <input
+              type="text"
+              className="registerInput"
+              placeholder="Enter your email..."
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              className="registerInput"
+              placeholder="Enter your password..."
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="registerButton" type="submit">
+              Register
+            </button>
+          </form>
+          <button className="registerLoginButton">
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          </button>
+          {error && <span style={{color:"red", marginTop:"10px"}}>Something went wrong!</span>}
+        </div>
+      );
+    }
