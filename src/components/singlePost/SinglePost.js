9ddec5e0cf6ad0f2d37b4f5fa15285b57c9a7eb9
connorb023 <connorcb@yahoo.com>
2023-05-22 16:58:21 -0600

    singlepost functions

--- src/components/singlePost/SinglePost.js
+++ src/components/singlePost/SinglePost.js
@@ -4,3 +4,14 @@ import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import "./singlePost.css";
+
+
+export default function SinglePost() {
+    const location = useLocation();
+    const path = location.pathname.split("/")[2];
+    const [post, setPost] = useState({});
+    const PF = "http://localhost:5000/images/";
+    const { user } = useContext(Context);
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+    const [updateMode, setUpdateMode] = useState(false);
