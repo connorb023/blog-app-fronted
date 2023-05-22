23ae101ff8f820e6cf2bb2b766e0a4aae6f40462
connorb023 <connorcb@yahoo.com>
2023-05-22 17:24:38 -0600

    functions

--- src/pages/settings/Settings.js
+++ src/pages/settings/Settings.js
@@ -3,3 +3,14 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import { useContext, useState } from "react";
 import { Context } from "../../context/Context";
 import axios from "axios";
+
+export default function Settings() {
+    const [file, setFile] = useState(null);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [success, setSuccess] = useState(false);
+  
+    const { user, dispatch } = useContext(Context);
+    const PF = "http://localhost:9000/images/"
+  
