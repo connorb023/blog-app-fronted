d3f8b1d04c62002c8dbae1bc4b9336d652166cd8
connorb023 <connorcb@yahoo.com>
2023-05-22 17:22:34 -0600

    functions

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -2,3 +2,24 @@ import axios from "axios";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./register.css";
+
+export default function Register() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setError(false);
+      try {
+        const res = await axios.post("/auth/register", {
+          username,
+          email,
+          password,
+        });
+        res.data && window.location.replace("/login");
+      } catch (err) {
+        setError(true);
+      }
+    };
