f232303504ccdb6eb4bd9a778fbc6c019bf9606f
connorb023 <connorcb@yahoo.com>
2023-05-22 17:26:59 -0600

    settings,password,email

--- src/pages/settings/Settings.js
+++ src/pages/settings/Settings.js
@@ -41,4 +41,60 @@ export default function Settings() {
         dispatch({ type: "UPDATE_FAILURE" });
       }
     };
-  
+    return (
+        <div className="settings">
+          <div className="settingsWrapper">
+            <div className="settingsTitle">
+              <span className="settingsUpdateTitle">Update Your Account</span>
+              <span className="settingsDeleteTitle">Delete Account</span>
+            </div>
+            <form className="settingsForm" onSubmit={handleSubmit}>
+              <label>Profile Picture</label>
+              <div className="settingsPP">
+                <img
+                  src={file ? URL.createObjectURL(file) : PF+user.profilePic}
+                  alt=""
+                />
+                <label htmlFor="fileInput">
+                  <i className="settingsPPIcon far fa-user-circle"></i>
+                </label>
+                <input
+                  type="file"
+                  id="fileInput"
+                  style={{ display: "none" }}
+                  onChange={(e) => setFile(e.target.files[0])}
+                />
+              </div>
+              <label>Username</label>
+              <input
+                type="text"
+                placeholder={user.username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <label>Email</label>
+              <input
+                type="email"
+                placeholder={user.email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label>Password</label>
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className="settingsSubmit" type="submit">
+                Update
+              </button>
+              {success && (
+                <span
+                  style={{ color: "green", textAlign: "center", marginTop: "20px" }}
+                >
+                  Profile has been updated...
+                </span>
+              )}
+            </form>
+          </div>
+          <Sidebar />
+        </div>
+      );
+    }
