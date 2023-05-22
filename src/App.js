74d0157acd15502c31e919a19eda2b9cf31f9f05
connorb023 <connorcb@yahoo.com>
2023-05-22 17:31:20 -0600

    paths

--- src/App.js
+++ src/App.js
@@ -9,4 +9,26 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { useContext } from "react";
 import { Context } from "./context/Context";
 
+function App() {
+  const { user } = useContext(Context);
+  return (
+    <Router>
+      <TopBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">{user ? <Home /> : <Register />}</Route>
+        <Route path="/login">{user ? <Home /> : <Login />}</Route>
+        <Route path="/write">{user ? <Write /> : <Register />}</Route>
+        <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+
 export default App;
