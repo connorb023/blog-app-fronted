4c72bfab916b676dcc959caf295070aa94e1dc77
connorb023 <connorcb@yahoo.com>
2023-05-23 07:46:40 -0600

    fixes

--- src/components/topbar/TopBar.js
+++ src/components/topbar/TopBar.js
@@ -25,11 +25,7 @@ export default function TopBar() {
               HOME
             </Link>
           </li>
-          <li className="topListItem">
-            <Link className="link" to="/">
-              ABOUT
-            </Link>
-          </li>
+        
           <li className="topListItem">
             <Link className="link" to="/">
               CONTACT
